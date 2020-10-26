db3ab76e59a46726a6787b2246cf46424ddcea8a
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-26 17:35:41 -0500

    completed delete function, next is edit

--- client/src/Components/ListTodos.js
+++ client/src/Components/ListTodos.js
@@ -1,9 +1,12 @@
 import React, { Fragment, useState, useEffect } from 'react'
+import EditTodo from './EditTodo'
 
 const ListTodos = () => {
 
     const [todos, setTodos] = useState([])
 
+
+    //get function
     const getTodos = async() => {
         try {
             const response = await fetch('http://localhost:5000/todos')
@@ -20,29 +23,36 @@ const ListTodos = () => {
         getTodos()
     }, [])
 
+    //delete function
+
+    const deleteTodo = async(id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: 'DELETE'
+            })
+
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
     return (
         <Fragment>
-    <table class="table mt-5 text-center">
+    <table className="table mt-5 text-center">
     <thead>
       <tr>
         <th>Description</th>
-        <th>Edit</th>
+        <th><EditTodo /></th>
         <th>Delete</th>
       </tr>
     </thead>
     <tbody>
-        {/*       
-        <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      */}
       {todos.map(todo =>(
-          <tr>
+          <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td>Edit</td>
-              <td>Delete</td>
+              <td><button className='btn btn-warning'>Delete</button></td>
+              <td><button className='btn btn-danger' onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
           </tr>
       ))}
     </tbody>
